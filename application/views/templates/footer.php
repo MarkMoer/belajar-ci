e0081d9c4f3a815fde85862537dab5e506320d75
MarkMoer <faris.kamaaluddin@gmail.com>
2024-01-30 16:10:09 +0700

    WPU EPS 10:26.49

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -50,6 +50,11 @@
 
 
 			<script>
+				$('.custom-file-input').on('change', function() {
+					let filename = $(this).val().split('\\').pop();
+					$(this).next('.custom-file-label').addClass("selected").html(filename);
+				});
+
 				$('.form-check-input').on('click', function() {
 					const menuId = $(this).data('menu');
 					const roleId = $(this).data('role');
@@ -69,6 +74,8 @@
 					});
 				});
 			</script>
+
+
 			</body>
 
 			</html>
