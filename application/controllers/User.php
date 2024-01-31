c4df5814a130bbc48255281eea6d54ee744dea7d
MarkMoer <faris.kamaaluddin@gmail.com>
2024-01-31 10:11:03 +0700

    WPU CI3:EPS11

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -42,7 +42,27 @@ class User extends CI_Controller
 			//cek jika ada gambar 
 			$upload_image = $_FILES['image']['name'];
 
+			if ($upload_image) {
+				$config['allowed_types'] = 'gif|jpg|png';
+				$config['max_size']      = '3000';
+				$config['upload_path']   = './assets/img/profile/';
 
+				$this->load->library('upload', $config);
+
+				if ($this->upload->do_upload('image')) {
+					$old_image = $data['user']['image'];
+					if ($old_image != 'default.jpg') {
+						unlink(FCPATH . 'assets/img/profile/' . $old_image);
+					}
+
+
+					$new_image = $this->upload->data('file_name');
+					$this->db->set('image', $new_image);
+				} else {
+					$this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">' . $this->upload->display_errors() . '</div>');
+					redirect('user');
+				}
+			}
 
 			$this->db->set('name', $name);
 			$this->db->where('email', $email);
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -5,12 +5,12 @@
 	<h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
 	<div class="row">
-		<div class="col-lg-6">
+		<div class="col-lg-8">
 			<?= $this->session->flashdata('message'); ?>
 		</div>
 	</div>
 
-	<div class="card mb-3" style="max-width: 540px;">
+	<div class="card mb-3 col-lg-8">
 		<div class="row g-0">
 			<div class="col-md-4">
 				<img src="<?= base_url('assets/img/profile/' . $user['image']); ?>" class="img-fluid rounded-start" alt="...">
